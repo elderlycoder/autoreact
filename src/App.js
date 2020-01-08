0941941d8aaa47cccd8a798e17d4227bef932461
elderlycoder <49819692+elderlycoder@users.noreply.github.com>
2020-01-08 09:47:48 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from './partials/Header'
+import AddOrder from './partials/AddOrder'
+import UserOrders from './partials/UserOrders'
 //import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+    <div>
       <Header />
-    </>
+      <Route path="/addorder" component={AddOrder} />
+      <Route path="/orders" component={UserOrders} />
+    </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/partials/AddOrder.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddOrder() {
+  return (
+    <div>
+      <p>Здесь форма добавления заказа</p>
+    </div>
+  );
+}
+
+export default AddOrder;
