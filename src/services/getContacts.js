e1754f6cd9b96c4457bdb5f897d8be81772449ee
Viacheslav Kozlov <vko@kodix.ru>
2020-01-18 23:04:09 +0300

    Получение контактов - запрос

--- src/services/getContacts.js
+++ src/services/getContacts.js
@@ -1,6 +1,6 @@
 export default class GetContacts {
    constructor(){
-      this.__apiBase = 'http://localhost:3000';
+      this.__apiBase = 'http://localhost:4000';
 
    }
    getResource = async (url) =>{
@@ -10,7 +10,7 @@ export default class GetContacts {
          throw new Error(`Could not fetch ${url}, received ${res.status}`)
       }
       // в fetch есть встроенный метод преобразования пришедшего в ответе json в объект js ".json()"
-      // т.к. метод .json() это асинхронная операция, и возвращает promise, нужно указать return(у) дождаться конца операции 
+      // т.к. метод .json() это асинхронная операция, и возвращает promise, нужно указать return(у) дождаться конца операции
       // с помощью await
       return await res.json();
    }
