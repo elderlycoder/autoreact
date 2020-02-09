d3ce49ec0dfbd091576a4ab63fc866b2bf664252
elderlycoder <49819692+elderlycoder@users.noreply.github.com>
2020-02-09 21:13:44 +0300

    3333

--- src/partials/Contacts/AddContact.js
+++ src/partials/Contacts/AddContact.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import ContactsServices from "../../services/Contacts";
+export default class AddContact extends Component {
+    Contacts = new ContactsServices();
+    state = {
+        // первоначальный state
+        contact: {},
+        error: false
+      };
 
-const AddContact = () => {
-  return (
-    <div>
-      <h4>Добавить контакт: </h4>
-      
-      <form action="contacts" method="post">
-      <div className="form-row">
-         <input type="text" name="name" placeholder="Имя" />
-         <input type="text" name="phone" placeholder="Телефон" />
-         <input type="text" id="model" name="model" placeholder="Марка и модель" /> 
-         <label> Год: <select id="year" name="year">
+
+    clearRefs() {
+        for (const key of Object.keys(this.refs)) {
+            if(this.refs[key].tagName !== "SELECT") {
+                this.refs[key].value = '';
+            }
+        }
+    }
+
+    AddContact = () => { 
+        const contact = {};
+        for (const key of Object.keys(this.refs)) {
+            contact[key] = this.refs[key].value
+        }
+
+
+        this.Contacts.addContact(contact)
+          .then(contact =>{
+              this.clearRefs();
+              this.props.contacts.push(contact);
+              this.props.updateContactsState(this.props.contacts)
+            })
+          .catch(console.log);
+      }
+
+
+    render() {
+        return (
+            <div>
+                <h4>Добавить контакт: </h4>
+
+                <div className="form-row">
+                    <input type="text" placeholder="Имя" ref="name"/>
+                    <input type="text" name="phone" placeholder="Телефон" ref="phone" />
+                    <input type="text" id="model" name="model" placeholder="Марка и модель" ref="car"/>
+                    <label> Год: <select id="year" name="year" ref = "year">
                         <option value="2019">2019</option>
                         <option value="2018">2018</option>
                         <option value="2017">2017</option>
@@ -28,16 +61,56 @@ const AddContact = () => {
                         <option value="2005">2005</option>
                         <option value="2004">2004</option>
                         <option value="2003">2003</option>
-                     </select>
-            <input type="text" id="vin" name="vin" placeholder="VIN код" />
-            <input type="text" name="descContact" placeholder="Примечание" />
-            <button>Сохранить</button>
-         </label>
-         </div>
-      </form>
-      
-    </div>
-  );
-};
+                    </select>
+                        <input type="text" id="vin" name="vin" placeholder="VIN код" ref="vin" />
+                        <input type="text" name="descContact" placeholder="Примечание" ref="notes"/>
+                        <button onClick={this.AddContact}>Сохранить</button>
+                    </label>
+                </div>
+            </div>
+        );
+    }
+}
+// import React from "react";
 
-export default AddContact;
+// const AddContact = () => {
+//   return (
+//     <div>
+//       <h4>Добавить контакт: </h4>
+      
+//       <form action="contacts" method="post">
+//       <div className="form-row">
+//          <input type="text" name="name" placeholder="Имя" />
+//          <input type="text" name="phone" placeholder="Телефон" />
+//          <input type="text" id="model" name="model" placeholder="Марка и модель" /> 
+//          <label> Год: <select id="year" name="year">
+//                         <option value="2019">2019</option>
+//                         <option value="2018">2018</option>
+//                         <option value="2017">2017</option>
+//                         <option value="2016">2016</option>
+//                         <option value="2015">2015</option>
+//                         <option value="2014">2014</option>
+//                         <option value="2013">2013</option>
+//                         <option value="2012">2012</option>
+//                         <option value="2011">2011</option>
+//                         <option value="2010">2010</option>
+//                         <option value="2009">2009</option>
+//                         <option value="2008">2008</option>
+//                         <option value="2007">2007</option>
+//                         <option value="2006">2006</option>
+//                         <option value="2005">2005</option>
+//                         <option value="2004">2004</option>
+//                         <option value="2003">2003</option>
+//                      </select>
+//             <input type="text" id="vin" name="vin" placeholder="VIN код" />
+//             <input type="text" name="descContact" placeholder="Примечание" />
+//             <button>Сохранить</button>
+//          </label>
+//          </div>
+//       </form>
+      
+//     </div>
+//   );
+// };
+
+// export default AddContact;
--- src/services/Contacts.js
+++ src/services/Contacts.js
@@ -1,4 +1,4 @@
-export default class GetContacts {
+export default class Contacts {
    constructor(){
       this.__apiBase = 'http://localhost:4000';
    }
@@ -14,8 +14,29 @@ export default class GetContacts {
       return await res.json();
    }
 
+   postData = async(url, body) => {
+      const options = {
+         method: 'POST', // *GET, POST, PUT, DELETE, etc.
+         headers: {
+             'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(body), // тип данных в body должен соответвовать значению заголовка "Content-Type"
+     }
+
+     return fetch(`${this.__apiBase}${url}`, options)
+   }
+
    getAllContacts = async () => {
       const contacts = await this.getResource('/api/v1/contacts')
       return contacts;
    }
+   addContact = async (contact) => {
+      const response = await this.postData('/api/v1/contacts', contact);
+
+      if (response.status !== 200) {
+         throw Error('Ошибка сохранения контакта', response.body);
+      }
+      return response.json();
+   }
+
 }
